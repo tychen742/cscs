635c641af818cd29571e9fd1857aec00a19f0539
Tsangyao Chen <tychen742@gmail.com>
2025-09-28 12:14:07 -0500

    committed @ 2025-0928-121407

--- bin/get-code-examples.sh
+++ bin/get-code-examples.sh
@@ -6,4 +6,4 @@ rm -rf $EXAMPLES/*
 
 # git clone https://github.com/LoyolaChicagoBooks/introcs-csharp-examples.git $EXAMPLES/introcs
 git clone https://github.com/mstbit/introcs-csharp-examples.git $EXAMPLES/introcs
-git clone https://bitbucket.org/josullivan1/comp170-xcode-xamarin-ios.git $EXAMPLES/ios
+# git clone https://bitbucket.org/josullivan1/comp170-xcode-xamarin-ios.git $EXAMPLES/ios
